5a6f85e360c2f95cd89b7fb81ae849806f7d1879
Naji Mohamed Omar <najimohamedomar034@gmail.com>
2026-05-24 23:05:20 +0300

    Update

--- script.js
+++ script.js
@@ -14,6 +14,68 @@ const products = [
         price: "Ksh.25",
         image: "image/Eggs.jpg"
     },
+    {
+        name: "Cakes",
+        price: "Ksh.70",
+        image: "image/Cake.jpg"
+    },
+    {
+        name: "Bread",
+        price: "Ksh.70",
+        image: "image/Bread.jpg"
+    }, {
+        name: "Tamu",
+        price: "Ksh.50",
+        image: "image/Tamu.png"
+    }, {
+        name: "Bread2",
+        price: "Ksh.50",
+        image: "image/Bread2.jpg"
+    }, {
+        name: "Milk",
+        price: "Ksh.40",
+        image: "image/Milk.jpg"
+    }, {
+        name: "U_fresh",
+        price: "Ksh.20",
+        image: "image/U_fresh.png"
+    }, {
+        name: "banana",
+        price: "Ksh.5",
+        image: "image/banana.jpg"
+    }, {
+        name: "Mango",
+        price: "Ksh.20",
+        image: "image/Mango.jpg"
+    }, {
+        name: "Tomatoes",
+        price: "Ksh.90",
+        image: "image/tomatoes.png"
+    }, {
+        name: "Pepper",
+        price: "Ksh.5",
+        image: "image/pepper.jpg"
+    }, {
+        name: "Onion_Red",
+        price: "Ksh.10",
+        image: "image/Onion-Red.jpg"
+    }, {
+        name: "Bell_pepper",
+        price: "Ksh.10",
+        image: "image/Bell_pepper.jpg"
+    }, {
+        name: "Garlic",
+        price: "Ksh.20",
+        image: "image/garlic.jpg"
+    }, {
+        name: "Lemon",
+        price: "Ksh.5",
+        image: "image/lemon.jpg"
+    }, {
+        name: "Potatoes",
+        price: "Ksh.80",
+        image: "image/potatoes.jpg"
+    },
 ];
 
 const productsContainer = document.getElementById("products");
